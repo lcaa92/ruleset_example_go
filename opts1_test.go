20db37e363d8d26f2e24ec058ab1761662cb6cb6
Leandro Araújo <leandrocaaraujo@gmail.com>
2020-06-11 12:26:51 -0300

    Adjust test code

--- opts1_test.go
+++ opts1_test.go
@@ -9,10 +9,16 @@ func TestOpts(t *testing.T) {
 	fmt.Println("Starting Tests...")
 
 	rs := NewRuleSet()
-	itemA := Item{}
-	itemB := Item{}
-	rs.AddDep(itemA, itemB)
-	rs.AddConflict(itemA, itemB)
-	rs.IsCoherent(itemA, itemB)
+	rs.AddDep("A", "B")
+	rs.AddDep("C", "D")
+	rs.AddConflict("D", "F")
+	rs.IsCoherent()
+
+	if rs.IsCoherent() {
+		fmt.Println("RuleSet is coherent")
+	} else {
+		fmt.Println("RuleSet is not coherent")
+	}
+
 	fmt.Println("Done Tests ...")
 }
