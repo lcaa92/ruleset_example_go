98fb27e23079f04f1597a5140131d4ce981dd70c
Leandro Araújo <leandrocaaraujo@gmail.com>
2020-06-11 12:25:46 -0300

    Alter RuleSet struct and functions

--- opts1.go
+++ opts1.go
@@ -2,39 +2,52 @@ package main
 
 import "fmt"
 
-// Item struct
-type Item struct{}
-
 // RuleSet struct
-type RuleSet struct{}
+type RuleSet struct {
+	Deps      [][2]string
+	Conflicts [][2]string
+}
 
 // NewRuleSet return empty RuleSet
 func NewRuleSet() RuleSet {
 	return RuleSet{}
 }
 
-// AddDep return nil
-func (rs RuleSet) AddDep(A Item, B Item) {
+// AddDep add dep in RuleSet
+func (rs *RuleSet) AddDep(A string, B string) {
 	fmt.Println("AddDep func")
+	rs.Deps = append(rs.Deps, [2]string{A, B})
 }
 
-// AddConflict return nil
-func (rs RuleSet) AddConflict(A Item, B Item) {
+// AddConflict add conflict in RuleSet
+func (rs *RuleSet) AddConflict(A string, B string) {
 	fmt.Println("AddConflict func")
+	rs.Conflicts = append(rs.Conflicts, [2]string{A, B})
 }
 
-// IsCoherent return nil
-func (rs RuleSet) IsCoherent(A Item, B Item) {
+// IsCoherent validate deps and conclicts RuleSet and return true or false
+func (rs *RuleSet) IsCoherent() (isCoherent bool) {
 	fmt.Println("IsCoherent func")
+	return
 }
 
 func main() {
 	fmt.Println("Starting ...")
 	rs := NewRuleSet()
-	itemA := Item{}
-	itemB := Item{}
-	rs.AddDep(itemA, itemB)
-	rs.AddConflict(itemA, itemB)
-	rs.IsCoherent(itemA, itemB)
+
+	rs.AddDep("A", "B")
+	rs.AddDep("C", "D")
+	rs.AddConflict("D", "F")
+	rs.IsCoherent()
+
+	if rs.IsCoherent() {
+		fmt.Println("RuleSet is coherent")
+	} else {
+		fmt.Println("RuleSet is not coherent")
+	}
+
+	fmt.Printf("%s", rs)
+	fmt.Println("")
+
 	fmt.Println("Done ...")
 }
